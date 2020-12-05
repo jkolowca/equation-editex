7a69d78548d033ee53e147abab183e2ad8fa296f
jkolowca <joanna.kolowca.mf13@gmail.com>
2020-12-05 05:02:32 +0100

    searching

--- src/app/components/editor/toolbar/search-box/search-box.component.ts
+++ src/app/components/editor/toolbar/search-box/search-box.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.scss']
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent {
+  @Output() filterChanged = new EventEmitter<string>();
+  form = new FormGroup({ filter: new FormControl('') });
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() { this.form.valueChanges.subscribe(v => this.filterChanged.emit(v.filter)); }
 }
--- src/app/components/editor/toolbar/toolbar.component.ts
+++ src/app/components/editor/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { parseEquation } from 'src/app/helpers/parsers';
 import { EquationService } from 'src/app/services/equation.service';
 import ToolbarContent from './toolbar.json';
@@ -8,15 +8,26 @@ import ToolbarContent from './toolbar.json';
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent {
   toolbar: any;
+  filter = false;
+  filtered: any;
 
   constructor(private equationService: EquationService) { this.toolbar = ToolbarContent; }
 
-  ngOnInit(): void {
-  }
-
   onClick(equation: any): void {
     this.equationService.addComponentsToEquation(parseEquation(equation));
+  }
+
+  onFilterChanged(filter: string): void {
+    if (!filter.length) {
+      this.filter = false;
+      return;
+    }
+    this.filtered = ToolbarContent.tabs.map(t => t.buttons)
+      .filter(t => t !== undefined)
+      .reduce((a, b) => a.concat(b), [])
+      .filter(b => b.display.includes(filter));
+    this.filter = true;
   }
 }
